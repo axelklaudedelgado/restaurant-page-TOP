58690c30935ffc11e0f5f385dd1ab144e54f09ad
axelklaudedelgado <144205486+axelklaudedelgado@users.noreply.github.com>
2024-10-03 12:12:53 +0800

    feat: add Webpack module rule for font files

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,13 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg)$/i,
         type: "asset/resource",
       },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i, 
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/fonts/[name][ext]', 
+        },
+      },
     ],
   },
 };
