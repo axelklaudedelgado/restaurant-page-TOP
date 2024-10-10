230e4aa66357726561e8d70db36470b6b1f5b508
axelklaudedelgado <144205486+axelklaudedelgado@users.noreply.github.com>
2024-10-10 10:36:33 +0800

    feat: add scroll-to-top behavior in tabLoader.js

--- src/utils/tabLoader.js
+++ src/utils/tabLoader.js
@@ -1,6 +1,7 @@
 export default function loadTab(tabFunction, param = null) {
     const contentDiv = document.querySelector('#content');
     contentDiv.innerHTML = '';
+    window.scrollTo(0, 0);
     const newContent = tabFunction(param);
     contentDiv.appendChild(newContent);
 }
