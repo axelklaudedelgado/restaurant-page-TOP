4893814b2a5265eeabcd79d8d701e67400f51e8f
axelklaudedelgado <144205486+axelklaudedelgado@users.noreply.github.com>
2024-10-10 11:18:12 +0800

    feat: apply active state function to nav links in header

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-import loadHome from './modules/home.js';
-import loadContact from './modules/contact.js';
-import { loadTab } from './utils/utils.js';
+import loadHome from "./modules/home.js";
+import loadContact from "./modules/contact.js";
+import { loadTab, handleActiveState } from "./utils/utils.js";
 
-import './styles/styles.css';
+import "./styles/styles.css";
 
-document.querySelector('#homeButton').addEventListener('click', () => loadTab(loadHome));
-document.querySelector('#contactButton').addEventListener('click', () => loadTab(loadContact));
+const navButtons = document.querySelectorAll(".navButtons");
+navButtons.forEach(nav => {
+    nav.addEventListener("click", () => {
+        handleActiveState(navButtons, nav, "activeNav");
+    });
+});
+
+document.querySelector("#homeButton").addEventListener("click", () => loadTab(loadHome));
+document.querySelector("#contactButton").addEventListener("click", () => loadTab(loadContact));
 
 loadTab(loadHome);
